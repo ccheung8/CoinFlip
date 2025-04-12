84d4efd676a50eefd71e27f7976dbc3868530b07
ccheung8 <calvincheung881@gmail.com>
2025-04-12 04:37:44 -0700

    did dice roll functionality and refactored update

---
+++ DiceRoll.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip {
+    internal class DiceRoll : IMiniGames {
+        private static Random random;
+        public string p1Result {  get; set; }
+        public string p2Result { get; set; }
+        public string Result { get; set; }
+
+        public DiceRoll() {
+            random = new Random();
+        }
+        public void Update() {
+            p1Result = Convert.ToString(random.Next(6) + 1);
+            p2Result = Convert.ToString(random.Next(6) + 1);
+
+            if (p1Result == p2Result) {
+                Result = "It's a Tie!";
+            } else if (Convert.ToInt32(p1Result) >= Convert.ToInt32(p2Result)) {
+                Result = "Player 1 Wins!";
+            } else {
+                Result = "Player 2 Wins!";
+            }
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -11,8 +12,10 @@ namespace CoinFlip
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private SpriteFont _font;
+        private Random _random;
 
-        private IMiniGames _miniGames;
+        private List<IMiniGames> _miniGames;
+        private IMiniGames _miniGame;
         private KeyboardState prevKbd;
 
         public Game1()
@@ -21,12 +24,15 @@ namespace CoinFlip
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            _miniGames = new RockPaperScissors();
+            _random = new Random();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _miniGames = new List<IMiniGames>() {
+                new RockPaperScissors(),
+                new DiceRoll()
+            };
 
             base.Initialize();
         }
@@ -43,12 +49,12 @@ namespace CoinFlip
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // updates when game is first run
-            if (gameTime.TotalGameTime == TimeSpan.Zero) _miniGames.Update();
+            // updates when pressing button or when game is first run
+            if ((Keyboard.GetState().IsKeyDown(Keys.Right) && prevKbd.IsKeyUp(Keys.Right))
+                    || gameTime.TotalGameTime == TimeSpan.Zero) {
+                _miniGame = _miniGames[_random.Next(2)];
 
-            // updates when pressing button
-            if (Keyboard.GetState().IsKeyDown(Keys.Right) && prevKbd.IsKeyUp(Keys.Right)) {
-                _miniGames.Update();
+                _miniGame.Update();
             }
 
             prevKbd = Keyboard.GetState();
@@ -62,10 +68,10 @@ namespace CoinFlip
 
             _spriteBatch.Begin();
 
-            _spriteBatch.DrawString(_font, "P1: " + _miniGames.p1Result, new Vector2(8), Color.Black);
-            _spriteBatch.DrawString(_font, "P2: " + _miniGames.p2Result, new Vector2(700, 8), Color.Black);
+            _spriteBatch.DrawString(_font, "P1: " + _miniGame.p1Result, new Vector2(8), Color.Black);
+            _spriteBatch.DrawString(_font, "P2: " + _miniGame.p2Result, new Vector2(700, 8), Color.Black);
 
-            _spriteBatch.DrawString(_font, _miniGames.Result, new Vector2(400, 240), Color.Black);
+            _spriteBatch.DrawString(_font, _miniGame.Result, new Vector2(400, 240), Color.Black);
 
             _spriteBatch.End();
 
