733dd706ad0fe4c009bade770d347bbcf0ed15f2
ccheung8 <calvincheung881@gmail.com>
2025-04-17 20:24:57 -0700

    added and completed text alignment functionality

--- Game1.cs
+++ Game1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -24,7 +25,10 @@ namespace CoinFlip
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
+            IsFixedTimeStep = false;
             _random = new Random();
+
+            this.Window.Title = "CoinFlip";  // sets window title
         }
 
         protected override void Initialize()
@@ -69,13 +73,28 @@ namespace CoinFlip
             _spriteBatch.Begin();
 
             _spriteBatch.DrawString(_font, "P1: " + _miniGame.p1Result, new Vector2(8), Color.Black);
-            _spriteBatch.DrawString(_font, "P2: " + _miniGame.p2Result, new Vector2(700, 8), Color.Black);
+            //_spriteBatch.DrawString(_font, "P2: " + _miniGame.p2Result, new Vector2(700, 8), Color.Black);
+            DrawStringAligned(_font, "P2: " + _miniGame.p2Result, "Right", Color.Black);
 
-            _spriteBatch.DrawString(_font, _miniGame.Result, new Vector2(400, 240), Color.Black);
+            //_spriteBatch.DrawString(_font, _miniGame.Result, new Vector2(400, 240), Color.Black);
+            DrawStringAligned(_font, _miniGame.Result, "Center", Color.Black);
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        // draws an aligned string based on alignment value passed
+        protected void DrawStringAligned(SpriteFont font, string text, string alignment, Color color) {
+            Vector2 textSize = font.MeasureString(text);
+            // values are casted to int from float to prevent text distortion from non int values
+            if (alignment.Equals("Right")) {
+                _spriteBatch.DrawString(font, text, new Vector2((int)(GraphicsDevice.Viewport.Width - textSize.X - 8), 8), color);
+            } else if (alignment.Equals("Center")) {
+                _spriteBatch.DrawString(font, text,
+                    new Vector2((int)((GraphicsDevice.Viewport.Width / 2) - (textSize.X / 2)), (int)(GraphicsDevice.Viewport.Height / 2)),
+                    color);
+            }
         }
     }
 }
